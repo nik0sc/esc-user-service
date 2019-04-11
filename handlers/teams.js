9f39a0ee76fc9da06fc6aaf6e231ce7a7e38e49c
Nikos Chan <nik0sc@users.noreply.github.com>
2019-04-12 02:03:47 +0800

    delete teams endpoint

--- handlers/teams.js
+++ handlers/teams.js
@@ -19,6 +19,8 @@ const validation = require('../validation');
  * }
  * 
  * Preconditions:
+ * - User is authenticated with session token
+ * - User is an admin
  * - Team name provided is not empty and not in use already
  * - Team name cannot be all digits (because that would look like an id)
  * 
@@ -96,6 +98,7 @@ exports.createTeam = async function (req, res) {
  * - Team exists
  * - User ident is valid
  * - User exists
+ * - User is authenticated with session token
  * - Calling user is an admin (yes, he can add himself)
  * - Target user is an admin
  * 
@@ -188,6 +191,7 @@ exports.addAdminToTeam = async function (req, res) {
  * - Team exists
  * - User ident is valid
  * - User exists
+ * - User is authenticated with session token
  * - Calling user is an admin (yes, he can remove himself)
  * - Target user is an admin
  * 
@@ -235,8 +239,8 @@ exports.removeAdminFromTeam = async function (req, res) {
         if (affected_rows === 1) {
             res.end();
         } else if (affected_rows === 0) {
-            res.json({
-                warning: 'This user was not in this team or there is no ' + 'such user or team'
+            res.status(404).json({
+                error: 'This user was not in this team or there is no ' + 'such user or team'
             });
         } else {
             res.status(500).json({
@@ -245,32 +249,11 @@ exports.removeAdminFromTeam = async function (req, res) {
             });
         }
     } catch (err) {
-        // Is this even needed?
-        switch (err.code) {
-            case 'ER_NO_REFERENCED_ROW':
-            case 'ER_BAD_NULL_ERROR':
-                // TODO make more specific
-                res.status(400).json({
-                    error: 'No such team or user or specified user is not admin'
-                });
-                break;
-        
-            case 'ER_DUP_ENTRY':
-                // This is fine
-                res.json({
-                    warning: 'This user is already in this team'
-                });
-                break;
-
-            default:
-                console.error(err);
-                res.status(500).json({
-                    error: 'Db error'
-                });
-                break;
-        }
+        console.error(err);
+        res.status(500).json({
+            error: 'Db error'
+        });
     }
-
 };
 
 /**
@@ -292,6 +275,7 @@ exports.removeAdminFromTeam = async function (req, res) {
  * - Team ident is valid
  * - Team exists
  * - Team has no tickets
+ * - User is authenticated with session token
  * - Calling user is an admin
  * 
  * Postconditions:
@@ -300,9 +284,119 @@ exports.removeAdminFromTeam = async function (req, res) {
  * - Idempotent
  */
 exports.deleteTeam = async function (req, res) {
+    console.log('Create new team');
+    const knex = req.app.locals.knex;
+    const ticket_axios = req.app.locals.ticket_axios;
 
+    // Validate team ident
+    const t_team_ident = req.body.team_ident;
+    const team_ident_validation = validation.validateTeamIdent(t_team_ident);
+
+    if (team_ident_validation === false) {
+        res.status(400).json({
+            error: 'Team identifier not provided or invalid'
+        });
+        return;
+    }
+
+    // Make sure this team exists first (and try to convert)
+    let exist_query = knex('teams')
+    .first('id')
+    .where(team_ident_validation, t_team_ident);
+
+    let team_id;
+    try {
+        team_id = await exist_query;
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error: 'Db error'
+        });
+        return;
+    }
+
+    if (typeof team_id === 'undefined' || team_id === null) {
+        res.status(404).json({
+            error: 'Could not find team with this identifier',
+            match: team_ident_validation,
+            ident: t_team_ident
+        });
+        return;
+    }
+
+    // Make sure there are no tickets associated with this team
+    try {
+        let {count} = ticket_axios.get(`/byTeam/${team_id}?type=count`);
+        if (count !== 0) {
+            res.status(400).json({
+                error: 'Team has tickets associated with it',
+                count: count
+            });
+            return;
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error: 'Ticket service error',
+            error_code: err.code
+        });
+        return;
+    }
+
+    // count == 0, go ahead and delete the team
+    // on delete cascade - m2m table rows are also deleted
+    let delete_team_query = knex('teams')
+        .delete()
+        .where('id', team_id)
+        .limit(1);
+    console.log(delete_team_query.toString());
+
+    let affected_rows;
+    try {
+        affected_rows = await delete_team_query;
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error: 'Db error'
+        });
+        return;
+    }
+
+    if (affected_rows === 0) {
+        console.error('Expected nonzero number of affected rows');
+        res.status(404).json({
+            error: 'No team was found'
+        });
+    } else {
+        res.end();
+    }
 };
 
+/**
+ * Get the teams that the current user is a member of
+ * 
+ * In: Headers
+ * - X-Parse-Session-Token: <String>
+ * 
+ * Out: 
+ *  Success: JSON
+ * [{
+ *  "id": <Number>,
+ *  "team_name": <String>
+ * }, ...]
+ *  Failure: JSON, status != 200
+ * {
+ *  "error": <String>
+ * }
+ * Preconditions:
+ * - User is authenticated with session token
+ * - Calling user is an admin
+ * 
+ * Postconditions:
+ * - Only the current user's teams are returned
+ * - No change to database
+ * - Idempotent
+ */
 exports.getCurrentAdminTeams = async function (req, res) {
 
 };
